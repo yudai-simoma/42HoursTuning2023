16202300e5a0e72e2e8c6ff5420668f7e1599c31
Enshin Andrei <b1os@bk.ru>
2023-06-18 07:14:03 +0900

    make more cache

--- app/node/src/routes/files/repository.ts
+++ app/node/src/routes/files/repository.ts
@@ -1,10 +1,40 @@
 import { RowDataPacket } from "mysql2";
-import pool from "../../util/mysql";
+import pool  from "../../util/mysql";
 import { File } from "../../model/types";
+
+
+declare global {
+  var pic_cache_paths: Map<string, File>;
+}
+
+
+
+//const [file] =  await pool.query<RowDataPacket[]>(
+  //"SELECT file_name, path FROM file"
+//);
+
 
 export const getFileByFileId = async (
   fileId: string
-): Promise<File | undefined> => {
+): Promise<File> => {
+
+  if (globalThis.pic_cache_paths == undefined ) {
+    globalThis.pic_cache_paths = await buildPicCache();
+  }
+  //console.log("lolo: ", globalThis.pic_cache_paths);
+  
+  let res;
+  if ( globalThis.pic_cache_paths != undefined && globalThis.pic_cache_paths.get(fileId) ) {
+    res = globalThis.pic_cache_paths.get(fileId);
+  } else {
+    res = {
+      fileName: 'aaa',
+      path: 'bbb',
+    }
+  }
+
+  return res as File;
+  /*
   const [file] = await pool.query<RowDataPacket[]>(
     "SELECT file_name, path FROM file WHERE file_id = ?",
     [fileId]
@@ -17,4 +47,29 @@ export const getFileByFileId = async (
     fileName: file[0].file_name,
     path: file[0].path,
   };
+  */
+};
+
+
+export const buildPicCache = async (): Promise < Map<string,File>> => {
+  const [file] = await pool.query<RowDataPacket[]>(
+    "SELECT file_name, path, file_id FROM file"
+  );
+  let res = new Map<string,File>();
+
+  if (file.length === 0) {
+    return res;
+  }
+
+  for (const f of file) {
+    res.set(f.file_id, {
+      fileName: f.file_name,
+      path: f.path,
+    })
+  }
+  return res;
+  //return {
+    //fileName: file[0].file_name,
+    //path: file[0].path,
+  //};
 };
