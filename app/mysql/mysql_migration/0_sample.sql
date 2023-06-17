02c7481e8c2a7c95356599615e6e08fda53629ca
Emu Kamada <es377233@gmail.com>
2023-06-17 17:49:43 +0900

    SELECT user_id FROM match_group_member WHERE match_group_id=10

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,1 +1,2 @@
 CREATE INDEX idx_match_group_all ON match_group(match_group_id, match_group_name, description, status, created_by, created_at);
+CREATE INDEX idx_match_group_member_group_id ON match_group_member(match_group_id);
